ac95772e64e4a558e6dae1f5ed627e1eae34fce4
jamespedwards42 <james.p.edwards42@gmail.com>
2016-11-04 10:36:36 -0700

    Simplify entrypoint.

--- cpu/entrypoint.sh
+++ cpu/entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e
 
-for arg in "$@" ; do
-  if [[ $arg == "-p"* ]] ; then
-    exec gosu nicehash /usr/local/bin/nheqminer_cpu "$@"
-  elif [[ $arg == "-h"* ]] ; then
-    exec gosu nicehash /usr/local/bin/nheqminer_cpu -h
-  fi
-done
-
-password="$(< /dev/urandom tr -dc 'A-Z-a-z-0-99!#$%&()*+,-./:;<=>?@[\]^_{|}~' | head -c 64)"
-exec gosu nicehash /usr/local/bin/nheqminer_cpu "$@" \
-  -p "$password"
+exec gosu nicehash /usr/local/bin/nheqminer_cpu "$@"
